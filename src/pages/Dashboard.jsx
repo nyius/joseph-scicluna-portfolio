0ffd2d61015be2c5de3aea6d8fa24fd569bcbb44
nyius <josephdesigned@gmail.com>
2022-04-28 16:22:31 -0400

    more styling

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { HiOutlineBadgeCheck } from 'react-icons/hi';
 import { BsBadge4KFill, BsBadgeHd, BsFillPinMapFill } from 'react-icons/bs';
 import { AiFillPhone, AiTwotoneMail } from 'react-icons/ai';
 import { DiGithubBadge } from 'react-icons/di';
-import badge from '../assets/insignia.png';
+import downArrow from '../assets/down_arrow.png';
 
 function Dashboard() {
 	// retro feel website (pixelated things, blocky, few rounded corners)
@@ -59,7 +59,7 @@ function Dashboard() {
 
 	//---------------------------------------------------------------------------------------------------//
 	return (
-		<div className="h-screen openingBg">
+		<div className="openingBg">
 			{/* <div className="h-1000 border-8 border-base-content">
 				<div className="typing-container border-8 w-full h-full flex flex-col justify-center items-center border-neutral-focus">
 					<div className="flex flex-row">
@@ -81,7 +81,7 @@ function Dashboard() {
 			<div id="stars2"></div>
 			<div id="stars3"></div>
 			{/* Cartridge */}
-			<div className="grid grid-cols-12 max-w-3xl mt-20 mx-auto w-full md:w-10/12 lg:w-8/12 xl:w-1/2 shadow-xl sticky top-0 left-0 cartridgeShadow">
+			<div className="grid grid-cols-12 z-10 max-w-3xl mt-14 sm:mt-44 mx-auto w-full md:w-10/12 lg:w-8/12 xl:w-1/2 shadow-xl sticky top-0 left-0 cartridgeShadow">
 				<div className="h-6 sm:h-10 col-span-11 rounded-t-xl bgTexture1 border-t-2 sm:border-t-8 border-l-2 sm:border-l-8 border-l-zinc-100/90 border-t-zinc-100/90 border-r-8 border-r-zinc-400/80"></div>
 				<div className="h-fit pb-10 sm:pb-0 sm:h-800 col-span-12 rounded-tr-xl rounded-b-xl bgTexture1 border-l-2 sm:border-l-8 border-l-zinc-100/90 border-r-2 sm:border-r-8 border-b-2 sm:border-b-8 border-r-zinc-400/80 border-b-zinc-400/80">
 					<div className=" bgTexture6 w-full h-2 sm:h-3 mt-3 sm:mt-8 border-t-2 border-t-zinc-100"></div>
@@ -99,25 +99,17 @@ function Dashboard() {
 											JOSEPH SCICLUNA
 										</p>
 									</div>
-									<div className="flex flex-row items-center gap-3 relative px-1 text-lg md:text-2xl">
-										<BsFillPinMapFill className="z-10" />
-										<p className="text-primary-content">Elmvale, On</p>
-									</div>
-									<div className="flex flex-row items-center gap-3 relative px-1 text-lg md:text-2xl">
-										<AiFillPhone className="z-10" />
-										<p className="text-primary-content">(705)-220-7676</p>
-									</div>
-									<div className="flex flex-row items-center gap-3 relative px-1 text-lg md:text-2xl">
-										<AiTwotoneMail className="z-10" />
-										<p className="text-primary-content text-lg xl:text-2xl">
-											josephdesigned@gmail.com
+									<div className="flex flex-row justify-center items-center my-8 gap-3 relative px-1 md:text-2xl">
+										<p className="pixelFont tracking-wider text-primary-content text-4xl ">
+											FULL-STACK WEB DEV
 										</p>
 									</div>
-									<div className="flex flex-row gap-4 w-full justify-between">
-										<HiOutlineBadgeCheck className="h-6 w-6 md:h-12 md:w-12 text-secondary" />
-										<BsBadge4KFill className="h-6 w-6 md:h-12 md:w-12 text-secondary" />
-										<BsBadgeHd className="h-6 w-6 md:h-12 md:w-12 text-secondary" />
-										<DiGithubBadge className="h-6 w-6 md:h-12 md:w-12 text-secondary cursor-pointer" />
+
+									<div className="flex flex-row gap-2 sm:gap-4 w-full justify-between">
+										<HiOutlineBadgeCheck className="h-8 w-8 md:h-12 md:w-12 text-secondary z-10" />
+										<BsBadge4KFill className="h-8 w-8 md:h-12 md:w-12 text-secondary z-10" />
+										<BsBadgeHd className="h-8 w-8 md:h-12 md:w-12 text-secondary z-10" />
+										<DiGithubBadge className="h-8 w-8 md:h-12 md:w-12 text-secondary cursor-pointer z-10" />
 									</div>
 								</div>
 							</div>
@@ -125,10 +117,20 @@ function Dashboard() {
 						</div>
 					</div>
 				</div>
+				<div className="hidden sm:block col-start-2 col-end-12 col-span-10 rounded-b-xl h-10 bgTexture6 grid grid-cols-12 shadow-xl border-l-4 border-l-base-300 "></div>
+			</div>
+
+			{/* MIDDLE TEXT  */}
+			<div className="w-full flex justify-center items-center mt-12 sm:mt-20 sm:mt-44 arrowBob-sm sm:arrowBob -z-10">
+				<img src={downArrow} className="w-20 h-30 sm:w-fit sm:h-fit" />
+			</div>
+			<div className="flex flex-col sm:font-bold text-center tracking-wides pixelFont gap-2 sm:gap-5 mt-5 sm:mt-10 mx-2 text-2xl sm:text-5xl mb-44 sm:mb-96">
+				<p className="text-secondary">STEP 1: BLOW ON BOTTOM OF CARTRIDGE</p>
+				<p className="text-secondary">STEP 2: INSERT TO CONTINUE...</p>
 			</div>
 
 			{/* Handheld Device */}
-			<div className="max-w-7xl mt-500 mx-auto w-full md:w-10/12 lg:w-8/12 xl:w-3/4 shadow-xl sticky top-0 left-0 deviceShadowUp z-10">
+			<div className="max-w-7xl mx-auto w-full md:w-10/12 lg:w-8/12 xl:w-3/4 shadow-xl sticky top-0 left-0 deviceShadowUp z-10">
 				<div className="deviceShadow">
 					<div className="rounded-2xl sm:rounded-4xl bgTexture7 h-screen w-full border-4 sm:border-8 border-l-zinc-100 border-t-zinc-100/70 border-r-zinc-400/50 border-b-zinc-400/50">
 						<div className="w-full grid grid-cols-12 h-20 rounded-t-2xl">
@@ -136,13 +138,13 @@ function Dashboard() {
 							<div className="col-start-12 col-span-1 w-3 sm:w-4 h-full bgTexture6 border-l-4 border-l-zinc-800 border-r-4 border-r-zinc-100"></div>
 						</div>
 						<div className=" bgTexture6 w-full h-3 sm:h-4 border-t-4 border-t-zinc-800 border-b-4 border-b-zinc-100"></div>
-						<div className="grid grid-cols-12 shadow-xl justify-center bgTexture1 z-20 items-center mt-20 h-1000 bg-zinc-900 mx-auto rounded-2xl w-11/12">
+						<div className="grid grid-cols-12 shadow-xl justify-center bgTexture1 z-20 items-center mt-20 h-96 sm:h-1000 bg-zinc-900 mx-auto rounded-2xl w-11/12">
 							<div className="col-span-1 h-full flex justify-center items-center z-20">
-								<div className="rounded-full bg-zinc-400 h-8 w-8"></div>
+								<div className="rounded-full bg-zinc-400 h-4 w-4 sm:h-8 sm:w-8"></div>
 							</div>
 							<div className="mx-auto col-span-10 rounded-xl z-20 text-accent h-11/12 w-full bg-gradient-to-r from-zinc-800 to-zinc-900 p-4 flex flex-col justify-center items-center">
 								<div className="flex flex-row ">
-									<span className="text-8xl font-bold">{letters.intro.word}</span>
+									<span className="text-3xl sm:text-8xl font-bold">{letters.intro.word}</span>
 									<span
 										className={`input-cursor ${
 											letters.intro.done ? 'hidden !important' : 'block'
@@ -150,13 +152,13 @@ function Dashboard() {
 									/>
 								</div>
 								<div className="flex flex-row">
-									<span className="text-6xl font-bold">{letters.name.word}</span>
+									<span className="text-2xl sm:text-6xl font-bold">{letters.name.word}</span>
 									<span
 										className={`input-cursor ${letters.name.done ? 'hidden !important' : 'block'} `}
 									/>
 								</div>
 								<div className="flex flex-row">
-									<span className="text-4xl font">{letters.whatIdo.word}</span>
+									<span className="text-xl sm:text-4xl font">{letters.whatIdo.word}</span>
 									<span className={`input-cursor`} />
 								</div>
 							</div>
@@ -166,8 +168,8 @@ function Dashboard() {
 				</div>
 			</div>
 
-			<div className="w-full h-96 bg-primary relBottomVert2 absolute left-0"></div>
-			<div className="w-full h-full bg-base-100 relBottomVert left-0 -z-10"></div>
+			<div className="w-full h-96 starsToAtmostphereGrad border-b-8 border-base-300 absolute left-0"></div>
+			{/* <div className="w-full h-full bg-secondary -z-10"></div> */}
 		</div>
 	);
 }
