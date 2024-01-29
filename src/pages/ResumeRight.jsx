f6209916e3f2b95eb49a15cd8cc05502d7eaca34
nyius <josephdesigned@gmail.com>
2024-01-28 23:06:33 -0500

    Updated resume page

--- src/pages/ResumeRight.jsx
+++ src/pages/ResumeRight.jsx
@@ -5,11 +5,15 @@ function ResumeRight({ slider }) {
 	//---------------------------------------------------------------------------------------------------//
 	return (
 		<>
-			<div className={`right-container-resume h-screen lg:h-full ${slider !== 3 ? '-z-50' : 'z-30'}`} style={slider === 3 ? { top: '0' } : { top: '-150vh' }}>
+			<div className={`right-container-resume h-screen lg:h-full ${slider !== 3 ? '-z-50' : 'z-30'}`} style={slider === 3 ? { top: '5vh' } : { top: '-150vh' }}>
 				<div className="flex lg-hidden pixel-font mb-4 text-white">Resume</div>
 				<object data={Resume} type="application/pdf" width="95%" height="90%">
 					<p>
-						Unable to display PDF file. <a href={Resume}>Download</a> instead.
+						Unable to display PDF file.{' '}
+						<a href={Resume} className="font-bold underline">
+							Download here
+						</a>{' '}
+						instead.
 					</p>
 				</object>
 			</div>
